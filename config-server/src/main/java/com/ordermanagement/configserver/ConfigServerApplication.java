c5d0b30edcb914353060a699836aaa9e1160e769
Sivareddy0930 <sivareddy0930@gmail.com>
2024-08-19 23:12:52 +0530

    config-server

--- config-server/src/main/java/com/ordermanagement/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/ordermanagement/configserver/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package com.ordermanagement.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class  ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
